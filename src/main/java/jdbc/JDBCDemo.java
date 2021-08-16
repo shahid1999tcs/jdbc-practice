bc5ecf078e3a4672ec9f4349d27e97770855ec57
shahid1999tcs <hellohbk29@gmail.com>
2021-08-16 12:26:06 +0000

    end of day push

--- src/main/java/jdbc/JDBCDemo.java
+++ src/main/java/jdbc/JDBCDemo.java
@@ -5,16 +5,29 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class JDBCDemo {
+	public static Logger logger = LogManager.getLogger(JDBCDemo.class);
 	public static void main(String[] args) {
+		logger.fatal("fatality");
 		String DB_URL = "jdbc:mysql://localhost/learn";
 		String DB_USER = "root";
 		String DB_PASSWORD = "Nuvelabs123$";
 		try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 				Statement statement = connection.createStatement();) {
 			// create(statement);
-			retrieve(statement);
+			//retrieve(statement);
+			//update(statement);
+			//delete(statement);
+			//List<String> regions = retrieveWithCondition(statement, "A");
+			//System.out.println(regions);
+			
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -22,11 +35,37 @@ public class JDBCDemo {
 		}
 	}
 
+	private static void delete(Statement statement) throws SQLException {
+		// TODO Auto-generated method stub
+		String sql = "DELETE FROM REGIONS WHERE REGION_ID=5";
+		statement.execute(sql);
+		
+	}
+
+	private static List<String> retrieveWithCondition(Statement statement, String string) throws SQLException {
+		String sql = "SELECT * FROM REGIONS WHERE REGION_NAME LIKE '" + string + "%' ORDER BY REGION_NAME DESC";
+		//System.out.println(sql);
+		ResultSet resultset = statement.executeQuery(sql);
+		List<String> regions = new ArrayList<String>();
+		while (resultset.next()) {
+			//System.out.println(resultset.getInt(1));// column index starts from 1
+			//System.out.println(resultset.getString(2));
+			regions.add(resultset.getString(2));
+		}
+		//System.out.println(regions);
+		return regions;
+	}
+
+	private static void update(Statement statement) throws SQLException {
+		// TODO Auto-generated method stub
+		statement.execute("UPDATE REGIONS SET REGION_NAME='Arctic' WHERE REGION_ID=5");
+	}
+
 	private static void retrieve(Statement statement) throws SQLException {
 
 		ResultSet resultset = statement.executeQuery("SELECT * FROM REGIONS");
 		while (resultset.next()) {
-			System.out.println(resultset.getInt(1));// column index starts from 1
+			logger.debug((resultset.getInt(1)));// column index starts from 1
 			System.out.println(resultset.getString(2));
 		}
 	}
